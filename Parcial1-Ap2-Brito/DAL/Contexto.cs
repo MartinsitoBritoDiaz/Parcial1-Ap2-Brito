fafb7909526741cf29827f7bc6b0afd625dbba52
Martinsito Brito Diaz <martinsito_brito@ucne.edu.do>
2020-06-10 14:58:26 -0400

    Add project files.

---
+++ Parcial1-Ap2-Brito/DAL/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1_Ap2_Brito.DAL
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}

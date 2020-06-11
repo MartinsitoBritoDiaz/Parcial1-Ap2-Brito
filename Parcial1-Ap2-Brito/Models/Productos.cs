8f3f49ab53a528aabb6c6762e92e715fefa25049
Martinsito Brito Diaz <martinsito_brito@ucne.edu.do>
2020-06-10 20:27:18 -0400

    Entidad terminada

---
+++ Parcial1-Ap2-Brito/Models/Productos.cs
@@ -0,0 +1,26 @@
+﻿using Parcial1_Ap2_Brito.Pages;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1_Ap2_Brito.Models
+{
+    public class Articulos
+    {
+        [Key]
+        public int articuloId { get; set; }
+
+        [Required(ErrorMessage ="Debe introducir la descripción")]
+        public string descripcion { get; set; }
+
+        [Required(ErrorMessage = "Debe introducir la existencia")]
+        public int existencia { get; set; }
+
+        [Required(ErrorMessage = "Debe introducir el costo")]
+        public double costo { get; set; }
+
+        public double valorInventario { get; set; }
+    }
+}

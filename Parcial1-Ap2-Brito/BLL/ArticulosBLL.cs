b233b20cf08256d9aec3661a998669abe8690dd4
Martinsito Brito Diaz <martinsito_brito@ucne.edu.do>
2020-06-10 20:46:47 -0400

    BLL  terminada

---
+++ Parcial1-Ap2-Brito/BLL/ArticulosBLL.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Parcial1_Ap2_Brito.DAL;
+using Parcial1_Ap2_Brito.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Parcial1_Ap2_Brito.BLL
+{
+    public class ArticulosBLL
+    {
+        public static bool Guardar(Articulos articulo)
+        {
+            if (!Existe(articulo.articuloId))
+                return Insertar(articulo);
+            else
+                return Modificar(articulo);
+        }
+
+        public static bool Existe(int id)
+        {
+            bool encontrado = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                encontrado = contexto.Articulo.Any(a => a.articuloId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return encontrado;
+        }
+
+        public static bool Insertar(Articulos articulo)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Articulo.Add(articulo);
+                paso = (contexto.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static bool Modificar(Articulos articulo)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Entry(articulo).State = EntityState.Modified;
+                paso = (contexto.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var articulo = contexto.Articulo.Find(id);
+
+                if(articulo != null)
+                {
+                    contexto.Articulo.Remove(articulo);
+                    paso = (contexto.SaveChanges() > 0);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static Articulos Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Articulos articulo;
+
+            try
+            {
+                articulo = contexto.Articulo.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return articulo;
+        }
+
+        public static List<Articulos> GetList(Expression<Func<Articulos , bool>> criterio)
+        {
+            List<Articulos> listaArticulos = new List<Articulos>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                listaArticulos = contexto.Articulo.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return listaArticulos;
+        }
+    }
+}
---
+++ Parcial1-Ap2-Brito/Migrations/20200611003110_Migracion.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parcial1_Ap2_Brito.Migrations
+{
+    public partial class Migracion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articulo",
+                columns: table => new
+                {
+                    articuloId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    descripcion = table.Column<string>(nullable: false),
+                    existencia = table.Column<int>(nullable: false),
+                    costo = table.Column<double>(nullable: false),
+                    valorInventario = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articulo", x => x.articuloId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Articulo");
+        }
+    }
+}
